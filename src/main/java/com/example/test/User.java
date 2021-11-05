db94a78a3c0c668c58b875177e3b8c7fd0ef2942
min <91645519+7rizL@users.noreply.github.com>
2021-11-05 17:09:43 +0900

    user update

---
+++ src/main/java/com/example/test/User.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+public class User {
+    private String name;
+    private int age;
+    private String nickname;
+
+    public User(String name, int age, String nickname) {
+        this.name = name;
+        this.age = age;
+        this.nickname = nickname;
+    }
+}
