99a0611900e425f92256e51e55222664aeb033ec
min <91645519+7rizL@users.noreply.github.com>
2021-11-05 17:07:06 +0900

    Initial commit

---
+++ src/main/java/com/example/test/Test.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+public class Test {
+    private String name;
+    private int age;
+
+    public Test(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
